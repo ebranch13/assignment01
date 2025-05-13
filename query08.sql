2a070caa34dd3c0261c135371d14014d95d8b2a5
ebranch13 <72311862+ebranch13@users.noreply.github.com>
2025-05-13 01:16:10 -0400

    number8

--- query08.sql
+++ query08.sql
@@ -1,30 +1,40 @@
 /*
-    Give the five most popular starting stations across all years between 7am
-    and 9:59am.
+    /*
+        Give the five most popular starting stations across all years between 7am
+        and 9:59am.
 
-    Your result should have 5 records with three columns, one for the station id
-    (named `station_id`), one for the point geography of the station (named
-    `station_geog`), and one for the number of trips that started at that
-    station (named `num_trips`).
-*/
+        Your result should have 5 records with three columns, one for the station id
+        (named `station_id`), one for the point geography of the station (named
+        `station_geog`), and one for the number of trips that started at that
+        station (named `num_trips`).
+    */
 
--- Enter your SQL query here
+    -- Enter your SQL query here
 
-
-SELECT
-    indego.station_statuses.id AS station_id,
-    ST_ASTEXT(indego.station_statuses.geog) AS station_geog,
-    COUNT(*) AS num_trips
-FROM (
-    SELECT * FROM indego.trips_2021_q3
-    UNION ALL
-    SELECT * FROM indego.trips_2022_q3
-) AS full_trips
-
-INNER JOIN indego.station_statuses
-    ON full_trips.start_station = indego.station_statuses.id::text
-
-WHERE EXTRACT(HOUR FROM full_trips.start_time) BETWEEN 7 AND 9
-GROUP BY indego.station_statuses.id, indego.station_statuses.geog
-ORDER BY COUNT(*) DESC
-LIMIT 5;
+    SELECT
+        stations.id AS station_id,
+        stations.geog AS station_geog,
+        COUNT(trips.trip_id) AS num_trips
+    FROM
+        (
+            SELECT
+                y1.start_station,
+                y1.trip_id
+            FROM indego.trips_2021_q3 AS y1
+            WHERE EXTRACT(HOUR FROM y1.start_time) >= 7 AND EXTRACT(HOUR FROM y1.start_time) < 10
+            UNION
+            SELECT
+                y2.start_station,
+                y2.trip_id
+            FROM indego.trips_2022_q3 AS y2
+            WHERE EXTRACT(HOUR FROM y2.start_time) >= 7 AND EXTRACT(HOUR FROM y2.start_time) < 10
+        ) AS trips
+    LEFT JOIN indego.station_statuses AS stations ON stations.id::TEXT = trips.start_station
+    GROUP BY
+        stations.id,
+        stations.geog
+    HAVING
+        stations.id IS NOT NULL
+    ORDER BY
+        num_trips DESC
+    LIMIT 5;
